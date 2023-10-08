cdf8b1aa312b2314ee736ae8a17939bbd015bd3f
bintangbind9 <bintangsaputra531@gmail.com>
2023-10-08 18:34:44 +0700

    Chapter 24

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,11 @@
+// Login Exceptions
+class InvalidUserCredentialAuthException implements Exception {}
+
+// Register Exceptions
+class WeakPasswordAuthException implements Exception {}
+class EmailAlreadyInUseAuthException implements Exception {}
+class InvalidEmailAuthException implements Exception {}
+
+// Generic Exceptions
+class GenericAuthException implements Exception {}
+class UserNotLoggedInAuthException implements Exception {}
