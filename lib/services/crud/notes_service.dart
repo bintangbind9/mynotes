4a9959db5d56c68522da23fd96c13ef3562e9069
bintangbind9 <bintangsaputra531@gmail.com>
2023-10-21 20:56:22 +0700

    Chapter 35

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -5,11 +5,14 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' show join;
 
 import 'crud_exceptions.dart';
+import '../../extensions/list/filter.dart';
 
 class NotesService {
   Database? _db;
 
   List<DatabaseNote> _notes = [];
+
+  DatabaseUser? _user;
 
   // Make this class singleton
   // What is singleton? that mean if create instance from this class, there is only once instance created in entire app.
@@ -25,14 +28,31 @@ class NotesService {
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else {
+          throw UserShouldBeSetBeforeReadingAllNotesException();
+        }
+      });
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) {
+        _user = user;
+      }
       return user;
     } on CouldNotFindUserException {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
     } catch (e) {
       rethrow;
