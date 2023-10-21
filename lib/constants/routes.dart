9940c41a7d1bcc7282364bb3bd4f9fc8d5b5cef3
bintangbind9 <bintangsaputra531@gmail.com>
2023-10-21 20:10:50 +0700

    Chapter 34

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,4 +2,4 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verify-email/';
-const newNoteRoute = '${notesRoute}new-note/';
+const createOrUpdateNoteRoute = '${notesRoute}new-note/';
