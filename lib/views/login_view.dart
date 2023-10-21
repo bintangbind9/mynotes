53888084d705cb37f9cdfac0e9f4ac2a53018088
bintangbind9 <bintangsaputra531@gmail.com>
2023-10-21 14:56:34 +0700

    Chapter 33

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../constants/routes.dart';
 import '../services/auth/auth_exceptions.dart';
 import '../services/auth/auth_service.dart';
-import '../utilities/show_error_dialog.dart';
+import '../utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
