b33aee468eeca4eea5796d2da462ea54aa1aff8f
bintangbind9 <bintangsaputra531@gmail.com>
2023-10-23 22:12:24 +0700

    Chapter 40 Part 1

---
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+
+import 'auth_state.dart';
+import 'auth_event.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    on<AuthEventInitialize>((event, emit) async {
+      await provider.initialize();
+      final user = provider.currentUser;
+      if (user == null) {
+        emit(const AuthStateLoggedOut());
+      } else if (!user.isEmailVerified) {
+        emit(const AuthStateNeedsVerification());
+      } else {
+        emit(AuthStateLoggedIn(user));
+      }
+    });
+  }
+}
